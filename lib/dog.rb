4749d3a119a9e1b0b6e842f72d22ffc2c26e1ef4
Connor Cowan <ccowan003@comcast.net>
2019-06-19 21:14:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
 
-  def name=(dog_name)           #name= is setter method, sets a value of an instance variable.
+  def name=(dog_name)           #(INSTANCE METHOD) name= is setter method, sets a value of an instance variable.
     @this_dogs_name = dog_name   #instance variable
   end
 
-  def name                    #reader method, responsible for reporting. Also known as getter method
+  def name                    #(INSTANCE METHOD) reader method, responsible for reporting. Also known as getter method
       @this_dogs_name
   end
 end
