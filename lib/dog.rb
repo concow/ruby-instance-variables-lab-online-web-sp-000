3104914300aca2a4c1423a7c1dca9c1c87dc578f
Connor Cowan <ccowan003@comcast.net>
2019-06-19 20:51:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,7 +4,7 @@ class Dog
     @this_dogs_name = dog_name   #instance variable
   end
 
-  def name                    #reader method, responsible for reporting. Also
+  def name                    #reader method, responsible for reporting. Also known as getter method
       @this_dogs_name
   end
 end
