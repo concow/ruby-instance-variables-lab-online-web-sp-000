4d4f77f15aba7a03a79021ad4652c07bfb2f7fe2
Connor Cowan <ccowan003@comcast.net>
2019-06-19 20:51:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
 
-  def name=(dog_name)           #name= is getter method
+  def name=(dog_name)           #name= is setter method, sets a value of an instance variable.
     @this_dogs_name = dog_name   #instance variable
   end
 
