5e0924467044053539dbbf931bbd30c6ec216042
Connor Cowan <ccowan003@comcast.net>
2019-06-19 21:19:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
 
   def name=(dog_name)           #(INSTANCE METHOD) name= is setter method, sets a value of an instance variable.
-    @this_dogs_name = dog_name   #instance variable. '@' makes it an instance
+    @this_dogs_name = dog_name   #instance variable. '@' defines an instance
   end
 
   def name          #(INSTANCE METHOD) getter/reader method, responsible for reporting. Without #getter, you can't retrieve a value of an instance variable outside the class the instance variable is instantiated from.
