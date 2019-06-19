ddae3d9dc895efe898e1bc38b6a0071d38b16d5e
Connor Cowan <ccowan003@comcast.net>
2019-06-19 20:47:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,11 +2,10 @@ class Dog
 
   def name=(dog_name)           #name= is getter method
     this_dogs_name = dog_name   #instance variable
-
   end
 
   def name
-
+      this_dogs_name
   end
 
 end
